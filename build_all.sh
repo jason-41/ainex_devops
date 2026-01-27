ddd403de37aed62982d4f4f4d4994819b12c6e02
nivilian <nivilian007@gmail.com>
2026-01-27 11:52:35 +0100

    chore: keep only required folders and files in branch

--- build_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "Building all LLM packages..."
-cd "$(dirname "$0")"
-colcon build --packages-select llm_msgs servo_service speech_interface llm_interface
-echo "Build complete!"
--- setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/ros/jazzy/setup.bash
-source install/setup.bash
-echo "Workspace sourced!"
